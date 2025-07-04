8d9cde6c7e4899aef5a18a453b9c46ade00e6dfe
pushpakpandya3292 <pushpakpandya3292@gmail.com>
2025-07-04 11:12:33 +0530

    feat : updated config file

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,11 @@ const nextConfig = {
     browsersListForSwc: true,
   },
   images: {
-    domains: ['localhost', 'bdde-2a09-bac5-3b08-1a46-00-29e-2c.ngrok-free.app'], // Add any other domains here
+    domains: [
+      "localhost",
+      "bdde-2a09-bac5-3b08-1a46-00-29e-2c.ngrok-free.app",
+      "bcda-2a09-bac5-3b0a-1a46-00-29e-fe.ngrok-free.app",
+    ], // Add any other domains here
   },
 };
 
