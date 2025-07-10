f660e34186986353b850804f5a460304e1cc3dbf
rc <rc@logixbuilt.com>
2025-07-10 11:21:15 +0530

    fix : ngrok url for images

--- next.config.mjs
+++ next.config.mjs
@@ -7,9 +7,7 @@ const nextConfig = {
   images: {
     domains: [
       "localhost",
-      "bdde-2a09-bac5-3b08-1a46-00-29e-2c.ngrok-free.app",
-      "bcda-2a09-bac5-3b0a-1a46-00-29e-fe.ngrok-free.app",
-      "55f4-2a09-bac1-36a0-28-00-1c5-db.ngrok-free.app",
+      "35c312c25a2c.ngrok-free.app"
     ], // Add any other domains here
   },
 };
