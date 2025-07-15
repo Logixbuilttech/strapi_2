d3c874fa51762d63bd8962e0cc82043b974e578b
rc <rc@logixbuilt.com>
2025-07-15 14:53:06 +0530

    worked on Carousel

--- components/comman/Home/WhyChooseUs.js
+++ components/comman/Home/WhyChooseUs.js
@@ -35,14 +35,6 @@ const WhyChooseUs = ({ data }) => {
   const mainTitle = renderRichText(parseStrapiRichText(data.title));
 
   // Owl options
-  const options = {
-    loop: true,
-    autoWidth: true,
-    margin: 0,
-    nav: false,
-    dots: false,
-    responsive: { 0: { items: 1 }, 768: { items: 1 } },
-  };
 
   return (
     <BackgroundBlock>
@@ -61,34 +53,41 @@ const WhyChooseUs = ({ data }) => {
 
       <div className="max-w-[1360px] w-full px-6 m-auto">
         <OwlCarousel
-          options={options}
+          options={null}
           ref={carousel}
+          autoWidth={true}
+          nav={false}
+          dots={false}
+          margin={12}
+          loop={true}
           className="owl-theme"
         >
           {Array.isArray(data.Item) &&
             data.Item.map((item) => (
-              <div
-                className="item bg-[rgba(255,255,255,.07)] rounded-[16px] p-6 flex flex-col gap-2 justify-between text-center items-center h-[342px] lg:h-[382px]"
-                key={item.id}
-                style={{ width: "auto" }}
-              >
-                <h4 className="uppercase text-white text-[24px] md:text-[28px] leading-[113%] ">
-                  {item.title}
-                </h4>
-                <span className="min-w-[24px] min-h-[24px] rounded-[8px] flex items-center justify-center m-auto bg-[rgba(255,255,255,.1)] p-2.5">
-                  {item.icon?.url && (
-                    <Image
-                      src={`${process.env.NEXT_PUBLIC_STRAPI_API_URL}${item.icon.url}`}
-                      alt={item.title}
-                      width={item.icon.width || 40}
-                      height={item.icon.height || 40}
-                      style={{ maxWidth: "100%", height: "auto" }}
-                    />
-                  )}
-                </span>
-                <p className="text-[#EEECDE] text-[18px] lg:text-[22px] font-medium leading-[120%] tracking-[-0.03em]">
-                  {item.text}
-                </p>
+              <div className="item">
+                <div
+                  className=" bg-[rgba(255,255,255,.07)] rounded-[16px] p-6 flex flex-col gap-2 justify-between text-center items-center h-[342px] lg:h-[382px] w-[300px] md:w-[334px] lg:w-[319px]"
+                  key={item.id}
+                  
+                >
+                  <h4 className="uppercase text-white text-[24px] md:text-[28px] leading-[113%] ">
+                    {item.title}
+                  </h4>
+                  <span className="min-w-[24px] min-h-[24px] rounded-[8px] flex items-center justify-center m-auto bg-[rgba(255,255,255,.1)] p-2.5">
+                    {item.icon?.url && (
+                      <Image
+                        src={`${process.env.NEXT_PUBLIC_STRAPI_API_URL}${item.icon.url}`}
+                        alt={item.title}
+                        width={item.icon.width || 40}
+                        height={item.icon.height || 40}
+                        style={{ maxWidth: "100%", height: "auto" }}
+                      />
+                    )}
+                  </span>
+                  <p className="text-[#EEECDE] text-[18px] lg:text-[22px] font-medium leading-[120%] tracking-[-0.03em]">
+                    {item.text}
+                  </p>
+                </div>
               </div>
             ))}
         </OwlCarousel>
